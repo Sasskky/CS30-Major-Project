c77cf2322d9145d5d69d2403b77c83258931f3d8
Sasskky <nickybro2005@gmail.com>
2023-01-11 12:07:56 -0600

    Made new button and finished start screen

--- sketch.js
+++ sketch.js
@@ -4,11 +4,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let button;
+let newBackground = "backgroubd2.jpg";
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+//let background2 = ;
 
 function draw() {
-  // square(20,20,20,20);
 }
+
+function setup() {
+  button = createButton("Start Game!");
+  width = 50;
+  button.position(935, 580);
+  button.mousePressed(switchToGameScreen);
+}
+
+function switchToGameScreen() {
+
+}
+
