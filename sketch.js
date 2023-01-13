0b2fd9d03c3fc2cbeb4b50a21a6ae6f93a28b0e3
Sasskky <nickybro2005@gmail.com>
2023-01-13 12:16:14 -0600

    commented css and worked on the javascript background

--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@
 // 11/17/22
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+//#5f8eb9
 let button;
 let newBackground = "backgroubd2.jpg";
 
 //let background2 = ;
 
 function draw() {
+  background("Lightblue");
 }
 
 function setup() {
@@ -20,6 +21,10 @@ function setup() {
 }
 
 function switchToGameScreen() {
+  console.log("here");
+  createCanvas(displayWidth, displayHeight);
+  
+  
 
 }
 
